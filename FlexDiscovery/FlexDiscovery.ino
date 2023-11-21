78a8e528dc657d63e6a8a8ca585c8996f6af3753
Massimo 'Max' Mucci <max.i8nhj@gmail.com>
2023-11-21 01:00:05 +0000

    it works

--- FlexDiscovery/FlexDiscovery.ino
+++ FlexDiscovery/FlexDiscovery.ino
@@ -1,14 +1,14 @@
 #include <Ethernet.h>
 #include <EthernetUdp.h>
-#define UDP_TX_PACKET_MAX_SIZE 544
-#define PAYLOAD_SIZE 516
+#define UDP_TX_PACKET_MAX_SIZE 546
+#define PAYLOAD_SIZE 518
 
-byte FlexMAC[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};    // Frisco 6400 MAC address
-IPAddress FlexIP(192, 168, 1, 177);                       // Frisco 6400 IP address
-IPAddress FlexRouterIP(60,128,1,1);                       // Frisco Gateway IP Address
+byte FlexMAC[] = {0x00, 0x1C, 0x2D, 0x05, 0x11, 0x10};    // Frisco 6400 MAC address
+IPAddress FlexIP(67, 198, 99, 60);                       // Frisco 6400 IP address
+IPAddress FlexRouterIP(67, 198, 99, 60);                       // Frisco Gateway IP Address
 
 byte ArduinoMAC[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-IPAddress ArduinoIP(10,19,8,164);
+IPAddress ArduinoIP(192,168,1,213);
 
 IPAddress Brodcast (255,255,255,255);
 unsigned int DiscoveryPort = 4992;      // local port to listen on
@@ -19,7 +19,7 @@ byte stream_id[4] = {0x00, 0x00, 0x08, 0x00};
 byte class_id[8] = {0x00, 0x00, 0x1C, 0x2D, 0x53, 0x4C, 0xFF, 0xFF};
 byte timestamp_int[4] = {0x65, 0x56, 0x3F,0xBF};
 byte timestamp_frac[8] = {0x00, 0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-char payload[PAYLOAD_SIZE] = "discovery_protocol_version=3.0.0.2 model=FLEX-6400 serial=2019-2544-6400-6026 version=3.5.9.22963 nickname=Frisco callsign=N5NHJ ip=10.19.8.53 port=4992 status=Available inuse_ip= inuse_host= max_licensed_version=v3 radio_license_id=00-1C-2D-05-11-10 requires_additional_license=0 fpc_mac= wan_connected=1 licensed_clients=2 available_clients=2 max_panadapters=2 available_panadapters=2 max_slices=2 available_slices=2 gui_client_ips= gui_client_hosts= gui_client_programs= gui_client_stations= gui_client_handles= ";
+char payload[PAYLOAD_SIZE] = "discovery_protocol_version=3.0.0.2 model=FLEX-6400 serial=2019-2544-6400-6026 version=3.5.9.22963 nickname=Locale callsign=I8NHJ ip=67.198.99.60 port=4992 status=Available inuse_ip= inuse_host= max_licensed_version=v3 radio_license_id=00-1C-2D-05-11-10 requires_additional_license=0 fpc_mac= wan_connected=1 licensed_clients=2 available_clients=2 max_panadapters=2 available_panadapters=2 max_slices=2 available_slices=2 gui_client_ips= gui_client_hosts= gui_client_programs= gui_client_stations= gui_client_handles= ";
 
 // An EthernetUDP instance to let us send and receive packets over UDP
 EthernetUDP Udp;
@@ -54,7 +54,7 @@ void loop() {
   Udp.write(class_id, sizeof(class_id));
   Udp.write(timestamp_int, sizeof(timestamp_int));
   Udp.write(timestamp_frac, sizeof(timestamp_frac));
-  Udp.write(payload, sizeof(header));
+  Udp.write(payload, sizeof(payload));
   Serial.println("Discovery Sent.");
   Udp.endPacket();
   delay(1000);
